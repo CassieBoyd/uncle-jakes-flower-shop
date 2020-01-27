d8822ec68892b7f81d37ff375a778e99304860ad
Cassie Boyd <cassandra.bacon@gmail.com>
2020-01-26 18:52:22 -0600

    Added flowers to flower directory

---
+++ flowers/rose.py
@@ -0,0 +1,10 @@
+from valentines_day import ValentinesDay
+
+class Rose:
+    pass
+
+# if __name__ == "__main__":
+#     for_beth = ValentinesDay()
+#     red_rose = Rose()
+
+#     for_beth.flowers.append(red_rose)
