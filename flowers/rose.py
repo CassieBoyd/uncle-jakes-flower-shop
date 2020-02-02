814c6a8db2a1dc82228ce7f6f8124ec6dec1c756
Cassie Boyd <cassandra.bacon@gmail.com>
2020-02-02 15:23:16 -0600

    Added inits to rose for imports

--- flowers/rose.py
+++ flowers/rose.py
@@ -1,6 +1,10 @@
+from attributes import IOrganic, IRefridgerated, IStemLength7
 
-class Rose:
+class Rose(IOrganic, IRefridgerated, IStemLength7):
     def __init__(self, name, color):
+        IOrganic.__init__(self)
+        IRefridgerated.__init__(self)
+        IStemLength7.__init__(self)
         self.name = name
         self.color = ["red", "pink", "blue"]
 
