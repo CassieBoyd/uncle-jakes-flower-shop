315e7e916c8b63169047fdf2d1856847538c0ebe
Cassie Boyd <cassandra.bacon@gmail.com>
2020-02-02 15:47:35 -0600

    Added attributes to daisy

--- flowers/daisy.py
+++ flowers/daisy.py
@@ -1,3 +1,9 @@
-class Daisy:
+from attributes import IOrganic, INonRefridgerated, IStemLength4
+
+
+class Daisy(IOrganic, INonRefridgerated, IStemLength4):
     def __init__(self,name):
+        IOrganic.__init__(self)
+        INonRefridgerated.__init__(self)
+        IStemLength4.__init__(self)
         self.name = name
