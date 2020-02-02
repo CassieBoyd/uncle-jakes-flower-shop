d6d6ffedbfec7b05a84d46b6c3b9c816b0fd12eb
Cassie Boyd <cassandra.bacon@gmail.com>
2020-02-02 15:49:27 -0600

    Added attributes to poppy

--- flowers/poppy.py
+++ flowers/poppy.py
@@ -1,3 +1,9 @@
-class Poppy:
+from attributes import IOrganic, INonRefridgerated, IStemLength4
+
+
+class Poppy(IOrganic, INonRefridgerated, IStemLength4):
     def __init__(self,name):
+        IOrganic.__init__(self)
+        INonRefridgerated.__init__(self)
+        IStemLength4.__init__(self)
         self.name = name
