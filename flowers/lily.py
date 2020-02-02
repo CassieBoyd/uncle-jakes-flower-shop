513f4121a9b18066beb7a8bf9724dcfd3fbee6a4
Cassie Boyd <cassandra.bacon@gmail.com>
2020-02-02 15:19:51 -0600

    Added inits to lily for imports

--- flowers/lily.py
+++ flowers/lily.py
@@ -1,3 +1,9 @@
-class Lily:
+from attributes import IOrganic, IRefridgerated, IStemLength7
+
+
+class Lily(IOrganic, IRefridgerated, IStemLength7):
     def __init__(self,name):
+        IOrganic.__init__(self)
+        IRefridgerated.__init__(self)
+        IStemLength7.__init__(self)
         self.name = name
